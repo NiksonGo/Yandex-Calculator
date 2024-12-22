41d21e634d9a8b51f667ae5249d886d6fe9adc85
Nikita <googlnik169gmail.com@mail.ru>
2024-12-22 19:11:17 +0300

    Fix issue with import of calculator package and update project structure

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module Yandex-Calculator
+module github.com/NiksonGo/Yandex-Calculator
 
 go 1.22.2
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -5,6 +5,7 @@ import (
  "net/http"
 
  "github.com/NiksonGo/Yandex-Calculator/internal/calculator"
+
 )
 
 type Request struct {
